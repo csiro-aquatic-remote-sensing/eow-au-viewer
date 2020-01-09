9b5f6e239428008fbbf22c843bd64655a9b8b2bd
Brooke Smith <brooke.b.smith@csiro.au>
2020-01-09 14:39:25 +1100

    Plot EOW in Waterbodies - icon in center of all reservoirs
    * currently its my gravatar

--- src/app/eow-data-piechart.ts
+++ src/app/eow-data-piechart.ts
@@ -73,42 +73,46 @@ export default class EOWDataPieChart {
   }
 
   draw(points: number[]) {
+    // Add a new overlay each time this is called
     if (points[0] !== null && points[1] !== null && !isNaN(points[0]) && !isNaN(points[1])) {
       this.log.info(theClass, `Draw pieChart at ${points[0]}, ${points[1]})}`);
-      if (!this.pieChartMap) {
-        this.pieChartMap = new Overlay({
-          element: this.htmlDocument.getElementById(htmlElementId),
-          position: points,
-          autoPan: true,
-          autoPanMargin: 275,
-          positioning: OverlayPositioning.CENTER_LEFT
-        });
-        this.map.addOverlay(this.pieChartMap);
-        const img = this.htmlDocument.createElement('img');
-        img.src = 'https://www.gravatar.com/avatar/0dbc9574f3382f14a5f4c38a0aec4286?s=60';
-        this.htmlDocument.getElementById(htmlElementId).appendChild(img);
-      } else {
-        setTimeout(() => {
-          this.pieChartMap.setPosition(points);
-        }, 200);
-      }
+      // if (!this.pieChartMap) {
+      //   this.pieChartMap = new Overlay({
+      const el = this.htmlDocument.createElement('div');
+      const img = this.htmlDocument.createElement('img');
+      el.setAttribute('id', '' + Math.random() * 1000);
+      img.src = 'https://www.gravatar.com/avatar/0dbc9574f3382f14a5f4c38a0aec4286?s=20';
+      el.appendChild(img);
+      this.htmlDocument.getElementById(htmlElementId).appendChild(el);
+      const pieChartMap = new Overlay({
+        element: el,
+        position: points,
+        autoPan: true,
+        autoPanMargin: 275,
+        positioning: OverlayPositioning.CENTER_LEFT
+      });
+      this.map.addOverlay(pieChartMap);
+
+      // pieChartMap.setPosition(points);
     }
-    // if (! this.pieChartMap) {
-    // this.pieChartMap = new Overlay({
-    // const container = this.htmlDocument.createElement('div');
-    // container.id = '' + Math.random() * 1000;
-    // const pieChartMap = new Overlay({
-    //   element: this.htmlDocument.getElementById(htmlElementId),
-    //   position: points,
-    //   autoPan: true,
-    //   autoPanMargin: 275,
-    //   positioning: OverlayPositioning.CENTER_LEFT
-    // });
-    // this.map.addOverlay(pieChartMap);
-    // const img = this.htmlDocument.createElement('img');
-    // img.src = 'https://www.gravatar.com/avatar/0dbc9574f3382f14a5f4c38a0aec4286?s=60';
-    // img.width = 20;
-    // container.appendChild(img);
-    // this.htmlDocument.getElementById(htmlElementId).appendChild(container);
   }
+
+  // if (! this.pieChartMap) {
+  // this.pieChartMap = new Overlay({
+  // const container = this.htmlDocument.createElement('div');
+  // container.id = '' + Math.random() * 1000;
+  // const pieChartMap = new Overlay({
+  //   element: this.htmlDocument.getElementById(htmlElementId),
+  //   position: points,
+  //   autoPan: true,
+  //   autoPanMargin: 275,
+  //   positioning: OverlayPositioning.CENTER_LEFT
+  // });
+  // this.map.addOverlay(pieChartMap);
+  // const img = this.htmlDocument.createElement('img');
+  // img.src = 'https://www.gravatar.com/avatar/0dbc9574f3382f14a5f4c38a0aec4286?s=60';
+  // img.width = 20;
+  // container.appendChild(img);
+  // this.htmlDocument.getElementById(htmlElementId).appendChild(container);
+
 }
