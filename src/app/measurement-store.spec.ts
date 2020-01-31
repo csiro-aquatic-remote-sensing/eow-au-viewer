615021af62c61e667acb8e45cd8e76fb30227f84
Brooke Smith <brooke@oehmsmith.com>
2020-01-31 22:04:00 +1100

    Plot EOW in Waterbodies - Fixed test runner and basic tests
    * Need to fix code and geometry-ops.spec
    * Add this to Extra Mocha Options in intellij > test dropdown >
    edit configurations > spanner (Edit templates)
    
    --require testHooks.js --require esm --require jsdom-global/register
    
    (same as in package.json > test-mocha script

--- src/app/measurement-store.spec.ts
+++ src/app/measurement-store.spec.ts
@@ -1,7 +1,10 @@
 import { MeasurementStore } from './measurement-store';
+import * as chai from 'chai';
+
+const expect = chai.expect;
 
 describe('MeasurementStore', () => {
   it('should create an instance', () => {
-    expect(new MeasurementStore()).toBeTruthy();
+    expect(new MeasurementStore(null)).is.not.undefined;  // tslint:disable-line
   });
 });
--- src/app/popup.spec.ts
+++ src/app/popup.spec.ts
@@ -1,7 +1,10 @@
 import { Popup } from './popup';
+import * as chai from 'chai';
+
+const expect = chai.expect;
 
 describe('Popup', () => {
   it('should create an instance', () => {
-    expect(new Popup(null, null, null)).toBeTruthy();
+    expect(new Popup(null, null, null)).is.not.undefined;  // tslint:disable-line
   });
 });
